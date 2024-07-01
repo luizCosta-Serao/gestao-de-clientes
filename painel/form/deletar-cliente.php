3171c6ca2a59923f7c3042c1ec5fdf14b2a6e9ac
Luiz Serão <luiz.serao@hotmail.com>
2024-07-01 06:38:22 -0300

    deletar cliente via ajax

---
+++ painel/form/deletar-cliente.php
@@ -0,0 +1,17 @@
+<?php
+  include('../../config.php');
+  if (Painel::isLogin() === false) {
+    die('Você não está logado!');
+  }
+  header('Content-Type: application/json');
+  $id = $_GET['id'];
+  echo $id;
+  $sql = MySql::connect()->prepare("DELETE FROM `clientes` WHERE id = ?");
+  $sql->execute(array($id));
+  if ($sql->rowCount() >= 1) {
+    echo json_encode($id);
+  } else {
+    echo json_encode('Ocorreu um erro');
+  }
+  
+  ?>
--- painel/js/index.js
+++ painel/js/index.js
@@ -31,22 +31,36 @@ function getClients() {
     contentType: "application/json; charset=utf-8",
     dataType: 'json',
   }).done(function(data) {
-    console.log(data);
-    for (let i = 0; i < data.length; i++) {
-      $('.box-client').prepend(`
-        <div class="box-single-client">
-          <h2>Nome: ${data[i].nome}</h2>
-          <p>Email: ${data[i].email}</p>
-          <p>Tipo: ${data[i].tipo}</p>
-          <p>CPF/CNPJ: ${data[i].cpf_cnpj}</p>
-        </div>
-      `)
-      
-      
+    if (typeof data === 'string') {
+      return $('.box-client').prepend(`
+        <p class="erro">Não existem clientes cadastrados</p>
+      `);
     }
+      for (let i = 0; i < data.length; i++) {
+        $('.box-client').prepend(`
+          <div class="box-single-client">
+            <h2>Nome: ${data[i].nome}</h2>
+            <p>Email: ${data[i].email}</p>
+            <p>Tipo: ${data[i].tipo}</p>
+            <p>CPF/CNPJ: ${data[i].cpf_cnpj}</p>
+            <a class="btn-delete" href="${location.href}?id=${data[i].id}">Deletar</a>
+          </div>
+        `)
+      }
   });
 }
 
 if(location.href.includes('listar-clientes')) {
   getClients();
+  const id = location.search.split('=')[1]
+  if (id) {
+    $.ajax({
+      url: 'http://localhost/gestao-clientes/painel/form/deletar-cliente.php',
+      method: 'GET',
+      data: {
+        id
+      }
+    })
+    location.href = 'http://localhost/gestao-clientes/painel/listar-clientes'
+  }
 }
