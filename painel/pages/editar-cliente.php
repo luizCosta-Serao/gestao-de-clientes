bd429b99ad2f5a9a521b98a796b34fba52fc14b5
Luiz Serão <luiz.serao@hotmail.com>
2024-07-01 07:55:51 -0300

    editar cliente via ajax

---
+++ painel/pages/editar-cliente.php
@@ -0,0 +1,18 @@
+<form id="editar-cliente" method="post">
+  <label for="name">Nome</label>
+  <input type="text" name="name" id="name">
+
+  <label for="email">Email</label>
+  <input type="email" name="email" id="email">
+
+  <label for="type">Tipo</label>
+  <select name="type" id="type">
+    <option value="fisico">Físico</option>
+    <option value="juridico">Jurídico</option>
+  </select>
+
+  <label for="cpf-cnpj">CPF/CNPJ</label>
+  <input type="text" name="cpf-cnpj" id="cpf-cnpj">
+
+  <input type="submit" name="action" value="Cadastrar">
+</form>
